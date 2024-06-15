8551f5c39a46158e0cb3568ed92ffd23871df69a
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-15 18:19:18 +0300

    Default HTML layout for Breadcrumbs

---
+++ app/components/Breadcrumbs/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Breadcrumbs = () => {
+  return (
+    <div>
+      <span>Main</span>
+      <span>-</span>
+      <span>Tarot</span>
+      <span>-</span>
+      <span>Yes or No answer</span>
+    </div>
+  );
+};
+
+export default Breadcrumbs;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
+import Breadcrumbs from "./components/Breadcrumbs";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Header />
+      <Breadcrumbs />
+    </div>
+  );
 };
 
 export default Homepage;
