371f0920d61dd2d5d43ec1d9dc012b0a09fbdddf
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-15 22:19:25 +0300

    Add YesOrNoAnswer component

---
+++ app/components/YesOrNoAnswer.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const YesOrNoAnswer = () => <div>Yes or No answer</div>;
+
+export default YesOrNoAnswer;
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import Breadcrumbs from "./components/Breadcrumbs";
 import TestGPTCall from "./components/TestGPTCall";
+import YesOrNoAnswer from "./components/YesOrNoAnswer";
 
 const Homepage = () => {
   return (
     <div>
       <Header />
       <Breadcrumbs />
-
+      <YesOrNoAnswer />
       <TestGPTCall />
     </div>
   );
