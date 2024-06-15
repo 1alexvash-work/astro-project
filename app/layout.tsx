49d53fdaa06eda36c74148652535014e18d24017
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-15 18:36:35 +0300

    Set up default page background and color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import colors from "./constants/colors";
 
 const poppins = Poppins({
   weight: ["400", "900"],
@@ -21,7 +22,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body
+        className={poppins.className}
+        style={{
+          background: colors.pageBackground,
+          color: colors.text.white,
+        }}
+      >
+        {children}
+      </body>
     </html>
   );
 }
