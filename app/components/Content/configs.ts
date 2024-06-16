7a50bf6af4dbeda8c79d6a6221e62526f96aa864
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-16 22:25:37 +0300

    Add page configs for Content component
    For better constants/configs alignment

---
+++ app/components/Content/configs.ts
@@ -0,0 +1,10 @@
+const configs = {
+  defaultPrompts: [
+    "Should I come back to my ex?",
+    "Will I get a promotion?",
+    "Do I know my soulmate already?",
+  ],
+  textAreaMaxLength: 250,
+};
+
+export default configs;
