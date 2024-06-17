871b9aa579c380096fed31dda3d5e5bd3d1294ac
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-17 09:26:15 +0300

    Activating modal on click

--- app/components/Content/DecodingText.tsx
+++ app/components/Content/DecodingText.tsx
@@ -3,6 +3,8 @@ import AvatarIcon from "@/images/avatar-icon.png";
 import bulp from "@/images/bulp.png";
 import Image from "next/image";
 import colors from "@/constants/colors";
+import useModal from "@/hooks/useModal";
+import Modal from "../UI/Modal";
 
 type Props = {
   answer: null | "yes" | "no";
@@ -20,81 +22,84 @@ const MiniLoadingSkeleton = () => (
   />
 );
 
-const DecodingText = ({ answer }: Props) => (
-  <div className="flex" style={{ marginTop: "200px" }}>
-    <Image
-      src={AvatarIcon}
-      alt="Avatar Icon"
-      width="32"
-      height="33"
-      className="mr-2"
-      style={{
-        alignSelf: "flex-start",
-      }}
-    />
-    {answer === null ? (
-      <MiniLoadingSkeleton />
-    ) : (
-      <div>
-        <div
-          style={{
-            padding: "12px",
-            borderRadius: "16px",
-            borderTopLeftRadius: 0,
-            background: "rgba(245,245,248, 0.15)",
-            marginBottom: "16px",
-          }}
-        >
-          It can be an indication of a fear of being excluded from the
-          conversation or a sign of intense worry for someone who may not be
-          showing any response. <br />
-          <br /> Dreams can also represent the turbulent and uncertain times
-          that can be difficult to navigate. They can serve as a warning of the
-          potential danger that lies ahead and can symbolize the chaos that
-          often follows.
-        </div>
-        <div className="flex justify-between">
-          <button
+const DecodingText = ({ answer }: Props) => {
+  const { isOpen, openModal, closeModal } = useModal();
+
+  return (
+    <div className="flex" style={{ marginTop: "200px" }}>
+      <Image
+        src={AvatarIcon}
+        alt="Avatar Icon"
+        width="32"
+        height="33"
+        className="mr-2"
+        style={{
+          alignSelf: "flex-start",
+        }}
+      />
+      {answer === null ? (
+        <MiniLoadingSkeleton />
+      ) : (
+        <div>
+          <div
             style={{
-              background: colors.pinkButtonBackground,
-              borderRadius: "15px",
-              width: "206px",
-              height: "50px",
-              fontSize: "18px",
-            }}
-            onClick={() => {
-              // ...
+              padding: "12px",
+              borderRadius: "16px",
+              borderTopLeftRadius: 0,
+              background: "rgba(245,245,248, 0.15)",
+              marginBottom: "16px",
             }}
           >
-            Get the decoding
-          </button>
-          <div className="flex">
-            <Image
-              src={bulp}
-              alt="bulp"
-              width="20"
-              height="21"
-              className="mr-2"
+            It can be an indication of a fear of being excluded from the
+            conversation or a sign of intense worry for someone who may not be
+            showing any response. <br />
+            <br /> Dreams can also represent the turbulent and uncertain times
+            that can be difficult to navigate. They can serve as a warning of
+            the potential danger that lies ahead and can symbolize the chaos
+            that often follows.
+          </div>
+          <div className="flex justify-between">
+            <button
               style={{
-                alignSelf: "flex-start",
+                background: colors.pinkButtonBackground,
+                borderRadius: "15px",
+                width: "206px",
+                height: "50px",
+                fontSize: "18px",
               }}
-            />
-            <p
-              style={{
-                color: "D3BDD9",
-                fontSize: "11px",
-                alignSelf: "flex-start",
-                opacity: 0.6,
-              }}
+              onClick={openModal}
             >
-              Use this tool as a clue; for the precise answer, <br /> consult
-              our tarot reader providing more data.
-            </p>
+              Get the decoding
+            </button>
+            <div className="flex">
+              <Image
+                src={bulp}
+                alt="bulp"
+                width="20"
+                height="21"
+                className="mr-2"
+                style={{
+                  alignSelf: "flex-start",
+                }}
+              />
+              <p
+                style={{
+                  color: "D3BDD9",
+                  fontSize: "11px",
+                  alignSelf: "flex-start",
+                  opacity: 0.6,
+                }}
+              >
+                Use this tool as a clue; for the precise answer, <br /> consult
+                our tarot reader providing more data.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    )}
-  </div>
-);
+      )}
+      {isOpen && <Modal onClose={closeModal} />}
+    </div>
+  );
+};
 
 export default DecodingText;
