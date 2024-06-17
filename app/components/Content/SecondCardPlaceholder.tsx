783da86cbf2a817d9b16fc1e703b63baa3f33536
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-17 08:30:01 +0300

    Add SecondCardPlaceholder

---
+++ app/components/Content/SecondCardPlaceholder.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import EmpressCard from "@/images/empress-card.png";
+import colors from "@/constants/colors";
+
+type Props = {
+  answer: null | "yes" | "no";
+};
+
+const MiniLoadingSkeleton = () => (
+  <div
+    className="animate-pulse bg-gray-500 w-20 inline-block"
+    style={{ height: "31px", borderRadius: "4px", opacity: 0.8 }}
+  />
+);
+
+const SecondCardPlaceholder = ({ answer }: Props) => (
+  <div className="text-center">
+    <div className="mb-6">
+      <h2 style={{ fontSize: "25px", fontWeight: 600, lineHeight: 1.3 }}>
+        Your question:
+      </h2>
+      <p className="pt-2 pb-3" style={{ opacity: 0.7 }}>
+        Should I come back to my ex?
+      </p>
+      {answer === null ? (
+        <MiniLoadingSkeleton />
+      ) : (
+        <div
+          style={{
+            fontSize: "35px",
+            color: colors.text.pink,
+            fontWeight: "bold",
+            lineHeight: 1,
+          }}
+        >
+          {answer.toUpperCase()}
+        </div>
+      )}
+    </div>
+    <Image src={EmpressCard} alt="Empress Card" width={216} height={400} />
+  </div>
+);
+
+export default SecondCardPlaceholder;
--- app/components/Content/index.tsx
+++ app/components/Content/index.tsx
@@ -5,12 +5,15 @@ import React, { ChangeEvent, useState } from "react";
 import configs from "./configs";
 import CardsPlaceholder from "./CardsPlaceholder";
 import QuestionSelector from "./QuestionSelector";
+import SecondCardPlaceholder from "./SecondCardPlaceholder";
 
 const Content = () => {
-  // TODO: state toggling
+  // TODO: add loading state
 
   const [question, setQuestion] = useState("");
   const [formStage, setFormStage] = useState(0);
+
+  const [answer, setAnswer] = useState<null | "yes" | "no">(null);
 
   const handleQuestionChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const newValue = event.target.value;
@@ -24,8 +27,7 @@ const Content = () => {
     <div className="grid grid-cols-12">
       <div className="col-span-5 flex flex-col gap-6 items-center">
         {formStage === 0 && <CardsPlaceholder />}
-        {/* {formStage === 1 && <SecondCardPlaceholder/>} */}
-        {/* {formStage === 2 && <SecondCardPlaceholder answer='yes'/>} */}
+        {formStage > 0 && <SecondCardPlaceholder answer={answer} />}
       </div>
 
       <div className="col-span-7 col-start-7 flex flex-col gap-8 flex-6">
