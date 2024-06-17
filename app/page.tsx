7485f3061fd3bbaeedb525cafcd1eee321646c86
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-17 13:03:07 +0300

    Update page container to collapse at mobile

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,28 @@ import Breadcrumbs from "./components/Breadcrumbs";
 import YesOrNoAnswer from "./components/YesOrNoAnswer";
 import Content from "./components/Content";
 
+const pageWidth = "1240px";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const PageContainer = ({ children }: Props) => (
+  <div
+    className="flex flex-col gap-6 mx-auto p-5"
+    style={{ width: pageWidth, maxWidth: "100%" }}
+  >
+    {children}
+  </div>
+);
+
 const Homepage = () => (
-  <>
-    <div
-      className="flex flex-col gap-6 mx-auto p-5"
-      style={{ width: "1240px" }}
-    >
-      <Header />
-      <Breadcrumbs />
-      <YesOrNoAnswer />
-      <Content />
-    </div>
-  </>
+  <PageContainer>
+    <Header />
+    <Breadcrumbs />
+    <YesOrNoAnswer />
+    <Content />
+  </PageContainer>
 );
 
 export default Homepage;
